99a95e2dee311f1ab7ffd523d91744d0faf65388
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-11-08 20:36:49 -0600

    Fixed indentation

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -7,20 +7,42 @@ export const Album = (props) => {
       <div className="album-card">
         <div className="album-card__inner">
           <div className = "album-cover">
-            <a href={props.urlAlbum} 
-               target= "_blank" 
-               rel="noopener noreferrer">
-                <img className="cover" src={props.image} alt={props.name}/>
+            <a 
+              href={props.urlAlbum} 
+              target= "_blank" 
+              rel="noopener noreferrer"
+            >
+              <img 
+                className="cover" 
+                src={props.image} 
+                alt={props.name}
+              />
             </a>
           </div>
           <div className="icon-container"> 
-            <img className="icon heart" src="/icons/heart.svg" alt="heart icon"/>
-            <img className="icon play" src="/icons/play.svg" alt="play button icon"/> 
-            <img className="icon dots" src="/icons/dots.svg" alt="dots icon"/> 
+            <img 
+              className="icon heart" 
+              src="/icons/heart.svg" 
+              alt="heart icon"
+            />
+            <img 
+              className="icon play" 
+              src="/icons/play.svg" 
+              alt="play button icon"
+            /> 
+            <img 
+              className="icon dots" 
+              src="/icons/dots.svg" 
+              alt="dots icon"
+            /> 
           </div> 
         </div> 
         <div>
-          <a href={props.urlAlbum} target= "_blank" rel="noopener noreferrer">  
+          <a 
+            href={props.urlAlbum} 
+            target= "_blank" 
+            rel="noopener noreferrer"
+          >  
             <h2 className="album-name">{props.name}</h2>
           </a>  
         </div>
